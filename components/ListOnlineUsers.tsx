e8cf28984d9e9fe62402ea451c83cf4c327b6d4f
Harshalvk <harshalvkhobragade@gmail.com>
2024-08-23 09:41:26 +0530

    ADD: Onclick handler, when click on user icon

--- components/ListOnlineUsers.tsx
+++ components/ListOnlineUsers.tsx
@@ -6,7 +6,7 @@ import Avatar from "./Avatar";
 
 const ListOnlineUsers = () => {
   const { user } = useUser();
-  const { onlineUsers } = useSocket();
+  const { onlineUsers, handleCall } = useSocket();
 
   const hasOtherOnlineUsers = onlineUsers?.some(
     (onlineUser) => onlineUser.profile.id !== user?.id
@@ -25,6 +25,7 @@ const ListOnlineUsers = () => {
           return (
             <div
               key={onlineUser.userId}
+              onClick={() => handleCall(onlineUser)}
               className="flex flex-col items-center gap-1 cursor-pointer"
             >
               <Avatar src={onlineUser.profile.imageUrl} />
